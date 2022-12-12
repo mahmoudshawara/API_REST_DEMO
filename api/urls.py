25324ae5d0bc672b591cf4689fe27fcd622a104f
mahmoudshawara <mahmoud.shawara@gmail.com>
2022-12-12 20:01:13 +0200

    Class Based View .. done

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from .views import AuthorViewSet , BookViewSet , FB_book ,FB_book_pk
+from .views import AuthorViewSet,BookViewSet ,FB_book ,FB_book_pk ,FB_author,FB_author_pk,CB_book,CB_book_pk,CB_author,CB_author_pk
 from django.conf.urls import include
 
 router = routers.DefaultRouter()
@@ -10,4 +10,10 @@ urlpatterns = [
     path('', include(router.urls)),
     path('FB_books/', FB_book),
     path('FB_books/<int:pk>', FB_book_pk),
+    path('FB_authors/', FB_author),
+    path('FB_authors/<int:pk>', FB_author_pk),
+    path('CB_books/', CB_book.as_view()),
+    path('CB_books/<int:pk>', CB_book_pk.as_view()),
+    path('CB_authors/', CB_author.as_view()),
+    path('CB_authors/<int:pk>', CB_author_pk.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ from .models import Author , Book
 from rest_framework import viewsets , status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from django.core.exceptions import ObjectDoesNotExist
 
 # GET POST DELETE PUT wit ViewSets.
 class AuthorViewSet(viewsets.ModelViewSet):
@@ -28,11 +30,12 @@ def FB_book(request):
             serializer.save()
             return Response (serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.data, status= status.HTTP_400_BAD_REQUEST)
+# GET PUT DELETE
 @api_view(['GET','PUT','DELETE'])
 def FB_book_pk(request,pk):
     try:
         book = Book.objects.get(pk=pk)
-    except Book.DoesNotExists: 
+    except Book.DoesNotExist: 
         return Response (status = status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         serializer = BookSerializer(book, many=False)
@@ -47,6 +50,103 @@ def FB_book_pk(request,pk):
         book.delete()
         return Response (status= status.HTTP_204_NO_CONTENT)
 
+@api_view(['GET','POST'])
+def FB_author(request):
+    if request.method == 'GET':
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors , many=True)
+        return Response (serializer.data)
+    elif request.method == 'POST':
+        serializer = AuthorSerializer (data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response (serializer.data , status = status.HTTP_201_CREATED)
+        return Response (serializer.data ,status = status.HTTP_400_BAD_REQUEST)
+@api_view(['GET','PUT','DELETE'])
+def FB_author_pk(request,pk):
+    try:
+        author = Author.objects.get(pk=pk)
+    except author.DoesNotExist:
+        return Response (status = sta.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = AuthorSerializer (author , many = False)
+        return Response (serializer.data)
+    elif request.method == 'PUT':
+        serializer = AuthorSerializer (author , request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response (serializer.data)
+        return Response (serializer.errors , status = status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        author.delete()
+        return Response (status =status.HTTP_204_NO_CONTENT)
+# class Based Views
+class CB_book(APIView):
+    def get(self,request):
+        books = Book.objects.all()
+        serializer = BookSerializer (books , many=True)
+        return Response (serializer.data)
+    def post(self,request):
+        serializer = BookSerializer (data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response (serializer.data , status=status.HTTP_201_CREATED)
+        return Response (serializer.errors , status =status.HTTP_400_BAD_REQUEST)
+class CB_book_pk(APIView):
+    def get_book(self,pk):
+        try :
+            book = Book.objects.get(pk=pk)
+            return book
+        except book.DoesNotExist:
+            return Response (status = status.HTTP_404_NOT_FOUND)
+    def get (self, request, pk):
+        book = self.get_book(pk)
+        serializer = BookSerializer (book , many=False)
+        return Response (serializer.data)
+    def put (self,request,pk):
+        book = self.get_book (pk)
+        serializer = BookSerializer (book,request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return (serializer.data)
+        return Response (serializer.errors , status =status.HTTP_400_BAD_REQUEST)
+    def delete (self,request,pk):
+        book = self.get_book(pk)
+        book.delete()
+        return Response (status = status.HTTP_204_NO_CONTENT)
+class CB_author(APIView):
+    def get(self,request):
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors , many=True)
+        return Response (serializer.data )
+    def post (self, request):
+        serializer = AuthorSerializer (data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response (serializer.data, status =status.HTTP_201_CREATED)
+        return Response (status=status.HTTP_400_BAD_REQUEST)
+class CB_author_pk(APIView):
+    def get_author(self,pk):
+        try:  
+            return Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            return Response (status = status.HTTP_404_NOT_FOUND)
+    def get (self , request , pk):
+        author = self.get_author(pk)
+        serializer = AuthorSerializer (author , many = False)
+        return Response (serializer.data)
+    def put (self , request , pk):
+        author = self.get_author(pk)
+        serializer = AuthorSerializer (author , request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response (serializer.data)
+        return Response (serializer.errors , status = status.HTTP_400_BAD_REQUEST)
+    def delete (self , request , pk):
+        author = self. get_author (pk)
+        author.delete()
+        return Response (status = status.HTTP_204_NO_CONTENT)
+        
 
 
 
